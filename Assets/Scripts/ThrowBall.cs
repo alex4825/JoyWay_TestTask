2c2a71fbd8043f60fce17766ffd9ae0997ae6e1c
Alexey Jukov <nitroalexus@gmail.com>
2024-07-19 00:20:17 +0300

    v_0.3.1__fire_particles
    Added fire effect on fire ball

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ThrowBall : MonoBehaviour
 {
     Rigidbody ballRb;
-    Quaternion rotation;
     float throwPower = 10f;
+    GameObject childFireObject;
+
     // Start is called before the first frame update
     void Start()
     {
+        childFireObject = transform.GetChild(0).gameObject;
         ballRb = GetComponent<Rigidbody>();
+        //throw at an angle of 30 degrees
         Vector3 direction = new Vector3(0, 0.5f, 1);
         ballRb.AddForce(throwPower * direction, ForceMode.Impulse);
     }
@@ -24,6 +27,8 @@ public class ThrowBall : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Bogeyman")
         {
+            //remove the parent (ball) from the fire so that it doesn't disappear immediately after removing the ball
+            childFireObject.transform.parent = null;
             Destroy(gameObject);
         }
         
